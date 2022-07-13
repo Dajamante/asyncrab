df00fa5b7d1c583e390bb7bb58f4b61daa9b0220
aissata <aimaiga2@gmail.com>
2022-07-13 15:35:51 +0200

    made it async

--- src/bin/kompass.rs
+++ src/bin/kompass.rs
@@ -7,7 +7,7 @@
 
 use nrf_embassy as _; // global logger + panicking-behavior
 
-use defmt::{info,Format};
+use defmt::info;
 use embassy::executor::Spawner;
 use embassy::time::{Delay, Duration, Timer};
 use embassy_nrf::twim::{self, Twim};
@@ -20,20 +20,23 @@ async fn main(_spawner: Spawner, p: Peripherals) {
     let irq = interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0);
     let i2c = Twim::new(p.TWISPI0, irq, p.P0_03, p.P0_04, config);
 
-    let mut hmc = HMC5983::new_with_interface(hmc5983_async::interface::I2cInterface::new(i2c));
-    hmc.init(&mut Delay).expect("mag_int init failed");
+    let mut hmc = HMC5983::new(i2c);
+
+    //new_with_interface(hmc5983_async::interface::I2cInterface::new(i2c));
+    hmc.init(&mut Delay).await.expect("init failed");
 
     loop {
-        // get roll and pitch estimate
-        if let Ok(temp) = hmc.get_temperature(){
-            info!("acc: {:?}", temp);
-        }
-        // get roll and pitch estimate
-        if let Ok(mag) = hmc.get_mag_vector(){
-            info!("mag: {:?}", mag);
-        }
-        
 
-        Timer::after(Duration::from_secs(10)).await;
+        // get roll and pitch estimate
+        if let Ok(temp) = hmc.get_temperature().await {
+            info!("Temperature: {:?}", temp);
+        }
+        // get roll and pitch estimate
+        match hmc.get_mag_vector().await {
+            Ok(mag) => info!("Magnitude vector: {:?}", mag),
+            Err(E) => info!("Printing Error {}", E),
+        }
+
+        Timer::after(Duration::from_secs(3)).await;
     }
 }
